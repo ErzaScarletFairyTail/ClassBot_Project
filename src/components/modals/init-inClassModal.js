7b87dea72da359ee4dc82793e72b604994dad045
ErzaScarletFairyTail <marcelin.trag@gmail.com>
2022-10-08 19:33:15 +0200

    InClassWork Update

--- src/components/modals/init-inClassModal.js
+++ src/components/modals/init-inClassModal.js
@@ -16,16 +16,41 @@ module.exports = {
         // Open the file timetable.json from the server folder
         const timetable = JSON.parse(fs.readFileSync(`./src/data/${interaction.guild.id}/timetable.json`));
 
-        // Add the new in class work to the timetable
-        timetable[week][day][time].inClassWork.Subject = subject;
-        timetable[week][day][time].inClassWork.Classroom = classroom;
+        
+        // IF THE DAY IS SATURDAY CHECK IF THE CLASS IS TERMINAL AND THE TIME IS BEETWEEN M1-M4
+        if (day.toUpperCase() === "SATURDAY" && timetable.data.class == "terminale") {
 
-        // Write the new timetable to the file timetable.json
-        fs.writeFileSync(`./src/data/${interaction.guild.id}/timetable.json`, JSON.stringify(timetable, null, 4));
+            if(time.toUpperCase() == "M1" || time.toUpperCase() == "M2" || time.toUpperCase() == "M3" || time.toUpperCase() == "M4") {
+                
+                timetable[week.toUpperCase()][day.toUpperCase()][time.toUpperCase()].inClassWork.Subject = subject;
+                timetable[week.toUpperCase()][day.toUpperCase()][time.toUpperCase()].inClassWork.Classroom = classroom;
+    
+                // Write the new timetable to the file timetable.json
+                fs.writeFileSync(`./src/data/${interaction.guild.id}/timetable.json`, JSON.stringify(timetable, null, 4));
+    
+                // Send a message to the user
+                await interaction.update({
+                    content: `The work has been added to the timetable for ${week} ${day} ${time}`,
+                    components: []
+                }); 
+            }
+            
 
-        // Send a message to the user
-        await interaction.reply({
-            content: `Added ${subject} in ${classroom} on ${day} at ${time} to the timetable`
-        });
+
+        } else {
+                
+                // Add the new in class work to the timetable
+                timetable[week.toUpperCase()][day.toUpperCase()][time.toUpperCase()].inClassWork.Subject = subject;
+                timetable[week.toUpperCase()][day.toUpperCase()][time.toUpperCase()].inClassWork.Classroom = classroom;
+    
+                // Write the new timetable to the file timetable.json
+                fs.writeFileSync(`./src/data/${interaction.guild.id}/timetable.json`, JSON.stringify(timetable, null, 4));
+    
+                // Send a message to the user
+                await interaction.update({
+                    content: `The work has been added to the timetable for ${week} ${day} ${time}`,
+                    components: []
+                });
+        }
     }
 };
